da83759180b7accabd61812a5bc9fa1aeb753cec
Igor Hatarist <igor@hatari.st>
2017-01-19 09:36:40 +0300

    Add database schema

---
+++ db/schema.sql
@@ -0,0 +1,37 @@
+CREATE TABLE scrobbles (
+    id integer NOT NULL,
+    user_id integer NOT NULL,
+    created_at timestamp with time zone NOT NULL,
+    played_at timestamp with time zone NOT NULL,
+    artist character varying(255) NOT NULL,
+    artist_id integer,
+    track character varying(255) NOT NULL,
+    track_id integer,
+    album character varying(255),
+    album_id integer,
+    tracknumber character varying(255),
+    length integer NOT NULL,
+    musicbrainz character varying(255),
+    source character varying(255),
+    rating character varying(255)
+);
+
+CREATE SEQUENCE scrobbles_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+;
+
+ALTER SEQUENCE scrobbles_id_seq OWNED BY scrobbles.id;
+ALTER TABLE ONLY scrobbles ALTER COLUMN id SET DEFAULT nextval('scrobbles_id_seq'::regclass);
+ALTER TABLE ONLY scrobbles ADD CONSTRAINT scrobbles_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY scrobbles ADD CONSTRAINT scrobbles_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id);
+ALTER TABLE ONLY scrobbles ADD CONSTRAINT scrobbles_artist_id_fkey FOREIGN KEY (artist_id) REFERENCES artists(id) ON DELETE SET NULL;
+
+CREATE INDEX scrobbles_artist_id_idx ON scrobbles USING btree (artist_id);
+CREATE INDEX scrobbles_artist_idx ON scrobbles USING btree (lower((artist)::text));
+CREATE INDEX scrobbles_played_at_idx ON scrobbles USING btree (played_at);
+CREATE INDEX scrobbles_track_idx ON scrobbles USING btree (lower((track)::text));
+CREATE UNIQUE INDEX scrobbles_unique_idx ON scrobbles (played_at, artist, track);
