46e26eac81399b2e8e40dfd663e34a5f320cc915
Igor Hatarist <igor@hatari.st>
2017-01-24 04:35:23 +0300

    Remove unusable/shitty maintenance code

--- scrobbler/commands/metadata.py
+++ scrobbler/commands/metadata.py
@@ -170,35 +170,6 @@ def find_similar_tracks(field_name, top_artists_count, chunks, start_from):
             db.session.commit()
 
 
-def fix_scrobble_length():
-    old_query = (
-        db.session.query(Scrobble.artist, Scrobble.track)
-        .filter(Scrobble.id < 134266)
-        .group_by(Scrobble.artist, Scrobble.track)
-    )
-
-    new_query = (
-        db.session.query(Scrobble.artist, Scrobble.track, func.avg(Scrobble.length))
-        .filter(Scrobble.id >= 134266)
-        .group_by(Scrobble.artist, Scrobble.track).all()
-    )
-
-    new_data = {'{0} - {1}'.format(*scrobble): int(scrobble[2]) for scrobble in new_query}
-
-    for pair in old_query:
-        name = '{0} - {1}'.format(*pair)
-        length = new_data.get(name, 0)
-
-        scrobbles = (
-            db.session.query(Scrobble)
-            .filter(Scrobble.id < 134266, Scrobble.artist == pair[0], Scrobble.track == pair[1])
-        )
-
-        scrobbles.update({'length': length})
-
-    db.session.commit()
-
-
 def download_artist_metadata(limit):
     artists = [obj[0] for obj in (
         db.session.query(Scrobble.artist).group_by(Scrobble.artist)
