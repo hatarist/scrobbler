a9e7a44e5fc3dc97e1fdf8d8dda47518a0a1c2b8
Igor Hatarist <igor@hatari.st>
2017-01-24 10:17:46 +0300

    Workaround Jinja2 template autoreload bug

--- scrobbler/webui/helpers.py
+++ scrobbler/webui/helpers.py
@@ -1,7 +1,7 @@
 import datetime
 from functools import wraps
 
-from flask import flash, redirect, request, url_for
+from flask import flash, request
 from flask_login import current_user
 
 from scrobbler import app, db, login_manager, __VERSION__
@@ -18,46 +18,6 @@ def admin_required(func):
             return app.login_manager.unauthorized()
         return func(*args, **kwargs)
     return decorated_view
-
-
-@app.template_filter('timesince')
-def timesince(time, now=None):
-    chunks = (
-        (60 * 60 * 24 * 365, 'year'),
-        (60 * 60 * 24 * 30, 'month'),
-        (60 * 60 * 24 * 7, 'week'),
-        (60 * 60 * 24, 'day'),
-        (60 * 60, 'hour'),
-        (60, 'minute'),
-        (1, 'second')
-    )
-
-    if not now:
-        now = datetime.datetime.now().replace(tzinfo=time.tzinfo)
-
-    delta = now - (time - datetime.timedelta(0, 0, time.microsecond))
-    since = delta.days * 24 * 60 * 60 + delta.seconds
-    if since <= 0:
-        return 'in the future'
-    for i, (seconds, name) in enumerate(chunks):
-        count = since // seconds
-        if count != 0:
-            break
-
-    if count > 1:
-        name += 's'
-
-    return '%(number)d %(type)s ago' % {'number': count, 'type': name}
-
-
-@app.template_filter('bignum')
-def bignum(num):
-    magnitude = 0
-    while abs(num) >= 1000:
-        magnitude += 1
-        num /= 1000.0
-    # add more suffixes if you need them
-    return '%.2f%s' % (num, ['', 'K', 'M'][magnitude]) if magnitude > 0 else num
 
 
 @app.context_processor
--- scrobbler/wsgi.py
+++ scrobbler/wsgi.py
@@ -2,10 +2,16 @@ from scrobbler import app, bcrypt, db, lastfm, login_manager
 from scrobbler.api.views import blueprint as api_bp
 from scrobbler.webui.views import blueprint as webui_bp
 
-# Blueprints
+# App config
 app.config.from_pyfile('config.py')
+
+# FIXME: Template filters (https://github.com/pallets/flask/issues/1907)
+import scrobbler.webui.filters  # noqa
+
+# Blueprints
 app.register_blueprint(api_bp)
 app.register_blueprint(webui_bp)
+
 
 # Database
 db.init_app(app)
